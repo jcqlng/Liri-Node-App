a2489ee6fbfcaafc792f03b481dd8623cc3ae029
jcqlng <jcqlngeiger@outlook.com>
2020-06-09 22:06:26 -0400

    dependencies added

--- liri.js
+++ liri.js
@@ -1,1 +1,8 @@
+// code to read and set any environment variables with the dotenv package 
 require("dotenv").config();
+
+// import the node-spotify-api ,api keys, request for npm, & fs package
+var Spotify = require("node-spotify-api");
+var keys = require("./keys");
+var request = require("request");
+var fs = require("fs");
