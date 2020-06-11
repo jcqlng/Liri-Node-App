1bd6369f4325bcbd6da6fbe9db437d958ee63731
jcqlng <jcqlngeiger@outlook.com>
2020-06-11 06:45:31 -0400

    completed spotify fucntion. Function breakdown and idea from Stack overflow's site

--- liri.js
+++ liri.js
@@ -7,15 +7,44 @@ var keys = require("./keys");
 var request = require("request");
 var fs = require("fs");
 
+//argv[2] chooses users actions; argv[3] is input parameter, ie; movie title
+var userCommand = process.argv[2];
+var secondCommand = process.argv[3];
+
+
 // initializes spotify api with the keys in .env file
 var spotify = new Spotify(keys.spotify);
 
 // function for spotify search 
 var spotifyThisSong = function(song){
-    if (!song){
-        song = "The sign Ace of Bass"
+    if (songName === undefined) {
+        songName = "What's my age again";
     }
-}
+
+    spotify.search(
+        {
+            type: "track",
+            query: userCommand
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
 
 var concertThis = function(artist){
     var region = ""
