12af4de8d3d23fcc2a2c0e565c1fbb234dc4e85a
jcqlng <jcqlngeiger@outlook.com>
2020-06-10 20:59:54 -0400

    started the Spotify and Concert search functions

--- liri.js
+++ liri.js
@@ -6,3 +6,21 @@ var Spotify = require("node-spotify-api");
 var keys = require("./keys");
 var request = require("request");
 var fs = require("fs");
+
+// initializes spotify api with the keys in .env file
+var spotify = new Spotify(keys.spotify);
+
+// function for spotify search 
+var spotifyThisSong = function(song){
+    if (!song){
+        song = "The sign Ace of Bass"
+    }
+}
+
+var concertThis = function(artist){
+    var region = ""
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+}
+
+
